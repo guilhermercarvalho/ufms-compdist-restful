7a3a85d938eed3b9b9f3320a6721a8763e68e410
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2020-12-01 01:01:54 +0000

    first test connection database

--- app/src/com.api/Movie.java
+++ app/src/com.api/Movie.java
@@ -1,3 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Movie {
-    
+    public static void main(String[] args) {
+        String jdbcURL = "jdbc:postgresql://db:5432/postgres";
+        String username = "postgres";
+        String password = "postgres";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Conectado");
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("Erro de conexão com PostgreSQL: " + e);
+            e.printStackTrace();
+        }
+    }
 }
