a4d0257f987c800a146ca394bb98cccd831a1b09
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2020-12-05 22:39:00 +0000

    add setters

--- app/src/model/MovieActor.java
+++ app/src/model/MovieActor.java
@@ -11,4 +11,12 @@ public class MovieActor {
     public Integer getActorid() {
         return actorid;
     }
+
+    public void setMovieid(Integer movieid) {
+        this.movieid = movieid;
+    }
+
+    public void setActorid(Integer actorid) {
+        this.actorid = actorid;
+    }
 }
