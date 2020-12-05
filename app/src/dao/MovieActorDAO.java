561087580994bcdf3478bc27fd078c3c00791cc4
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2020-12-05 22:38:26 +0000

    create searchMovieActors function

--- app/src/dao/MovieActorDAO.java
+++ app/src/dao/MovieActorDAO.java
@@ -2,34 +2,44 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import db.DatabaseConnection;
 import model.MovieActor;
 
 public class MovieActorDAO {
     private Connection connection;
-    
+
     public MovieActorDAO() {
         this.connection = new DatabaseConnection().getConnection();
     }
 
-    public void search(MovieActor movieActor) throws SQLException {
-        String sql = "SELECT FROM movie_actor WHERE movieid=? AND actorid=?";
-
+    public List<MovieActor> searchMovieActors(int id) {
         try {
-            PreparedStatement stmt = connection.prepareStatement(sql);
+            List<MovieActor> mas = new ArrayList<MovieActor>();
+            PreparedStatement stmt = this.connection.prepareStatement(
+                "SELECT * FROM movie_actor WHERE movieid=?"
+            );
+            
+            stmt.setInt(1, id);
+            
+            ResultSet result = stmt.executeQuery();
 
-            stmt.setInt(1, movieActor.getMovieid());
-            stmt.setInt(2, movieActor.getActorid());
+            while(result.next()) {                
+                MovieActor ma = new MovieActor();
+                ma.setActorid(result.getInt("actorid"));
+                
+                mas.add(ma);
+            }
 
-            stmt.execute();
-
+            result.close();
             stmt.close();
+            return mas;
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            connection.close();
         }
     }
 
