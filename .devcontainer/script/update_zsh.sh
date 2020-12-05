089776989c364183ca95f622613184f6bbaf833a
Guilherme Carvalho <guilhermercarvalho512@gmail.com>
2020-12-05 22:34:19 +0000

    add alias to compile and execute java files

--- .devcontainer/script/update_zsh.sh
+++ .devcontainer/script/update_zsh.sh
@@ -31,7 +31,8 @@ EOF
 
 cat << EOF >> $HOME/.zshrc
 
-alias cjava="javac -cp /workspace/app/lib/json.jar:/workspace/app/lib/postgresql.jar:. -d /workspace/app/bin \$(find /workspace/app/src/* | grep .java)"
-alias ejava="java -cp .:/workspace/app/lib/postgresql.jar:/workspace/app/lib/json.jar: server.Server -t"
+alias cjava="javac -cp /workspace/app/lib/json.jar:/workspace/app/lib/postgresql.jar:. -d /workspace/app/bin \\$(find /workspace/app/src/* | grep .java)"
+alias ejava="cd /workspace/app/bin && java -cp .:/workspace/app/lib/postgresql.jar:/workspace/app/lib/json.jar: server.Server -t && cd -"
+alias cejava="cjava && ejava"
 EOF
 
